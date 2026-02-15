14ef871df1df653cd77678e42d2b464cd4ae9b54
Lakshmi Dubey <lakshmi.mcahbti@gmail.com>
2026-02-14 22:39:30 -0600

    added another approach

--- remove-duplicates-unsorted-array-brute-force.cs
+++ remove-duplicates-unsorted-array-brute-force.cs
@@ -33,3 +33,53 @@ namespace HelloWorld
 			}
 		}
 	}
+
+
+///Another way
+/// O(n^2) time complexity and O(n) space complexity
+/// using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace HelloWorld
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+	
+			int[] arr={7,-1,-1,-2,0,1,1,8,8,2,-1,3,3,8,4,4,9,4,5};
+		
+			//unsorted array
+			//remove dupliactes
+			//O(n^2)
+     List<int> uniqueEleArray=new List<int>();
+     
+			for(int i=0;i<arr.Length-1;i++){
+			  bool found=false;
+			  for(int j=0;j<uniqueEleArray.Count;j++){
+			    
+			    //check where the arry elem is present in uniqueEleArray
+			    //if yes then the element is found 
+			    //berak
+			    if(arr[i]==uniqueEleArray[j])
+			    {
+			      found=true;
+			      break;
+			    }
+			  }
+			  if(!found){
+			    uniqueEleArray.Add(arr[i]);
+			  }
+  		
+			}
+		Console.Write("[");
+			  foreach(int a in uniqueEleArray){
+			    Console.Write(a+",");
+			  }
+			      	Console.Write("]");
+			}
+	  
+	}
+	}
